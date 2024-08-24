ab7e4581d74d06f3b10173be6033d0606583c204
MohammadRezaeezadeh <marzam6484@gmail.com>
2024-08-24 14:16:56 +0330

    از اینجا داستان شروع میشود
    اقا مشکلات tiled  حل شد

---
+++ git1/Assets/SuperTiled2Unity/Examples/Overhead/Maps/MyWorld/overworld.tsx
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="overworld" tilewidth="16" tileheight="16" tilecount="1440" columns="40">
+ <image source="X:/unityProject/-Shadow-of-Pigeon-Poori/git1/Assets/cos&amp;sher/gfx/Overworld.png" width="640" height="576"/>
+ <tile id="0">
+  <properties>
+   <property name="path" value=""/>
+  </properties>
+ </tile>
+ <wangsets>
+  <wangset name="path2" type="mixed" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="1160" wangid="0,0,1,1,1,0,0,0"/>
+   <wangtile tileid="1161" wangid="0,0,1,1,1,1,1,0"/>
+   <wangtile tileid="1162" wangid="0,0,0,0,1,1,1,0"/>
+   <wangtile tileid="1200" wangid="1,1,1,1,1,0,0,0"/>
+   <wangtile tileid="1201" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="1202" wangid="1,0,0,0,1,1,1,1"/>
+   <wangtile tileid="1240" wangid="1,1,1,0,0,0,0,0"/>
+   <wangtile tileid="1241" wangid="1,1,1,0,0,0,1,1"/>
+   <wangtile tileid="1242" wangid="1,0,0,0,0,0,1,1"/>
+   <wangtile tileid="1280" wangid="1,1,1,0,1,1,1,1"/>
+   <wangtile tileid="1281" wangid="1,1,1,1,1,0,1,1"/>
+   <wangtile tileid="1320" wangid="1,0,1,1,1,1,1,1"/>
+   <wangtile tileid="1321" wangid="1,1,1,1,1,1,1,0"/>
+  </wangset>
+ </wangsets>
+</tileset>
--- git1/Assets/scripte/CameraMovement.cs
+++ git1/Assets/scripte/CameraMovement.cs
@@ -15,13 +15,13 @@ public class CameraMovement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void LateUpdate()
     {
         if(transform.position!=target.position)
         {
             Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
             targetPosition.x = Mathf.Clamp(targetPosition.x, minPosition.x, maxPosition.x);
-            targetPosition.y = Mathf.Clamp (targetPosition.y, minPosition.y, maxPosition.y);
+            targetPosition.y = Mathf.Clamp(targetPosition.y, minPosition.y, maxPosition.y);
             transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
         }
     }
