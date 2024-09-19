57afcaaf432164ed6cd08a4996ffc0a1116a52f2
MohammadRezaeezadeh <marzam6484@gmail.com>
2024-09-19 20:49:25 +0330

    چرت پرت

--- git1/Assets/World/Overworld.tsx
+++ git1/Assets/World/Overworld.tsx
@@ -1,9 +1,15 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="fist" tilewidth="16" tileheight="16" tilecount="1440" columns="40">
- <image source="../../cos&amp;sher/gfx/Overworld.png" width="640" height="576"/>
+<tileset version="1.10" tiledversion="1.11.0" name="Overworld" tilewidth="16" tileheight="16" tilecount="1440" columns="40">
+ <image source="../cos&amp;sher/gfx/Overworld.png" width="640" height="576"/>
+ <tile id="123" probability="0.1"/>
+ <tile id="124" probability="0.1"/>
+ <tile id="125" probability="0.1"/>
+ <tile id="163" probability="0.1"/>
+ <tile id="164" probability="0.1"/>
  <wangsets>
-  <wangset name="Unnamed Set" type="corner" tile="-1">
-   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+  <wangset name="soil" type="corner" tile="-1">
+   <wangcolor name="soil" color="#ff0000" tile="-1" probability="1"/>
+   <wangcolor name="grasses" color="#00ff00" tile="-1" probability="1"/>
    <wangtile tileid="1160" wangid="0,0,0,1,0,0,0,0"/>
    <wangtile tileid="1161" wangid="0,0,0,1,0,1,0,0"/>
    <wangtile tileid="1162" wangid="0,0,0,0,0,1,0,0"/>
