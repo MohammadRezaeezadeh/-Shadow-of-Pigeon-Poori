5131e9f31f7b4849facc2716809ed005f92fd20d
MohammadRezaeezadeh <marzam6484@gmail.com>
2024-09-19 21:05:29 +0330

    man

--- git1/Assets/scripte/RoomMove.cs
+++ git1/Assets/scripte/RoomMove.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class RoomMove : MonoBehaviour
     
@@ -10,10 +9,6 @@ public class RoomMove : MonoBehaviour
     public Vector2 cameraChange;
     public Vector3 playerChange;
     private CameraMovement cam;
-    public bool needText;
-    public string placeName;
-    public GameObject text;
-    public Text placeText;
     void Start()
     {
         cam =Camera.main.GetComponent<CameraMovement>();
@@ -31,17 +26,6 @@ public class RoomMove : MonoBehaviour
             cam.minPosition += cameraChange;
             cam.maxPosition += cameraChange;
             other.transform.position += playerChange;
-            if(needText)
-            {
-                StartCoroutine(placeNameCo());
-            }
         }
-    }
-    private IEnumerator placeNameCo()
-    {
-        text.SetActive(true);
-        placeText.text = placeName;
-        yield return new WaitForSeconds(4f);
-        text.SetActive(false);
     }
 }
